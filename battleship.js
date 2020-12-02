b2a50a8771724b5efc35e2949351cc79f8a3855f
Paller020 <paller21016@columbushs.comgit config --global user.name Your>
2020-12-02 09:38:21 -0500

    starting

---
+++ battleship.js
@@ -0,0 +1,24 @@
+var board = [
+[null, null, null, "S", null],
+[null, "S", null, "S", null],
+["S", null, null, null, null],
+[null, "S", null, null, null],
+[null, null, null, null, "S"],
+];
+
+var firstRow = board[0];
+console.log("First Row: " + firstRow);
+
+var secondRow = board[1];
+console.log("Second Row: " +secondRow);
+
+var thirdRow = board[2];
+console.log("Third Row: " + thirdRow);
+
+var fourthRow = board[3];
+console.log("Fourth Row: " + thirdRow);
+
+var fifthRow = board[4];
+console.log("Fifth Row: " + thirdRow);
+
+
